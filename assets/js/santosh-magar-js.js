332b28960329c84ba397baa160fb565175293887
Santosh Thapa Magar <santosh.magar@yipl.com.np>
2021-12-27 16:11:41 +0545

    update 12-27-2021

---
+++ assets/js/santosh-magar-js.js
@@ -0,0 +1,58 @@
+// --------site logo change in every 2 sec---
+let site_logo=document.getElementById('site-logo');
+let logos=['logo-stmp-001.png','logo-stmp-002.png','logo-stmp-003.png','logo-stmp-004.png'];
+var x = -1;
+setInterval(function () {
+  x = (x === logos.length - 1) ? 0 : x + 1;
+  site_logo.src='assets/images/'.concat(logos[x])
+},2000);
+
+// -------------------------------------
+
+var btn_know_more = document.getElementById("know_more");
+btn_know_more.onclick = function () {
+  var val = 'knowModal';
+  var modal = document.getElementById(val);
+  modal.style.display = "block";
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  let close="close_".concat(val);
+  var span = document.getElementsByClassName(close)[0];
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+}
+
+var btn_map_popup = document.getElementById("map_popup");
+btn_map_popup.onclick = function () {
+  var val = 'mapModal';
+  var modal = document.getElementById(val);
+  modal.style.display = "block";
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  let close="close_".concat(val);
+  var span = document.getElementsByClassName(close)[0];
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+}
+// ---------------------------------------------------
+$(".header-nav").each(function(){
+  var toggleUD = $(this).find('.ham-list');
+  toggleUD.addClass("ham-hide");
+  $(this).find('.ham').click(function(){
+    toggleUD.toggleClass("ham-hide");
+  })
+});
+
+$(".ham-list li").click(function(){
+  $(".ham-list").addClass("ham-hide");
+});
+
+// ----------------------------------------------------
